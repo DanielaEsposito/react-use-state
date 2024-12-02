82230171cf7ad95de58bc9e29e46121b7a8f85f5
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-12-02 14:02:45 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+//import { useState } from 'react'
+import Header from "./componensts/Header"
+import Main from "./componensts/Main"
+import Footer from "./componensts/Footer"
+
+function App() {
+  
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
